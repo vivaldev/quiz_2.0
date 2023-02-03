61f7ae5ec0b728ec1606dd348d64c0b923b36826
vivaldev91 <valtteri.virtanen.dev@gmail.com>
2023-02-04 00:32:31 +0200

    Added reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,48 @@ import React from "react";
 import { useState, useReducer, useRef } from "react";
 import { Flex, Heading, Text, Input } from "@chakra-ui/react";
 
+const quizData = [
+  {
+    question: "What is the capital of Finland?",
+    answers: ["Helsinki", "Tampere", "Pori", "Rauma"],
+    correctAnswer: "Pori",
+  },
+  // more questions here
+];
+
+const initialState = {
+  playerName: "",
+  questions: quizData,
+  currentQuestion: 0,
+  score: 0,
+};
+
+function quizReducer(state, action) {
+  switch (action.type) {
+    case "PLAYER_NAME":
+      return {
+        ...state,
+        playerName: action.payload,
+      };
+
+    case "NEXT_QUESTION":
+      return {
+        ...state,
+        currentQuestion: state.currentQuestion + 1,
+      };
+    case "UPDATE_SCORE":
+      return {
+        ...state,
+        score: state.score + action.payload,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(quizReducer, initialState);
+  const answeInputRef = useRef(null);
   return (
     <Flex
       w="100vw"
@@ -15,7 +56,20 @@ const App = () => {
       <Heading as="h1" color="whiteAlpha.900" mt="5vh">
         The Quiz Game!
       </Heading>
-      <Flex w="50%" h="60%" border="5px double wheat" mt="5vh"></Flex>
+      <Flex
+        flexDirection="column"
+        justify="center"
+        align="center"
+        w="50%"
+        h="60%"
+        border="5px double wheat"
+        mt="5vh"
+      >
+        <Heading as="h3" color="green.600">
+          What's your name?
+        </Heading>
+        <Input type="text" w="60%" m={5} bg="red.100" />
+      </Flex>
     </Flex>
   );
 };
